b74aa6ee08ec24e50a300e92af2f36e7b0ee90f9
vinicius <vinicius.faleiros@hotmail.com>
2021-03-12 10:35:52 -0300

    handleCreateNewTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,14 +17,28 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     //Math.random()
+
     if (newTaskTitle == '') {
       alert('Por favor preencha o campo de nova todo');
       return;
     }
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(tasks => [...tasks, newTask])
+
   }
+
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+   
+
+
   }
 
   function handleRemoveTask(id: number) {
